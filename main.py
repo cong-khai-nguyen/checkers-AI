8b35bf04342857120dbf6449703291d247e73f61
Cong Nguyen <nguyencongkhai1407@gmail.com>
2022-03-28 15:29:06 -0500

    finish drawing the board with checker pieces

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from checkers.constants import WIDTH, HEIGHT
+from checkers.board import Board
+
 
 FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -8,9 +10,17 @@ pygame.display.set_caption("Checkers")
 def main():
     run = True
     clock = pygame.time.Clock()
+    board = Board()
 
     while run:
         clock.tick(FPS)
-        pass
 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+        board.draw(WIN)
+        pygame.display.update()
+    pygame.quit()
 main()
